8aced8d37f04dbe5ec743aa75c190cc9a2fda84c
ouraan <rizhaalfianita1412@gmail.com>
2024-03-05 22:29:06 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      croissantOne: ['"Croissant One"', "serif"],
+      poppins: ["Poppins", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        whiten: "#F4F5F7",
+        lightVintage: "#E7DFD0",
+        vintage: "#9A8354",
+        darkBlue: "#08182F",
+        mediumBlue: "#15253F",
+        babyBlue: "#455D83",
+        softBlue: "#687C9F",
+      },
+    },
+  },
+  plugins: [],
+};
