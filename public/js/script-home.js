6b4c3b2f243985874b7351dd03bd70bbcd30ef75
ouraan <rizhaalfianita1412@gmail.com>
2024-03-24 21:10:19 +0700

    update form

--- public/js/script-home.js
+++ public/js/script-home.js
@@ -20,38 +20,50 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-function handleIntersection(entries, observer) {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      // Add a class to trigger the transition
-      entry.target.classList.add("opacity-100");
-    } else {
-      // Remove the class if not intersecting
-      entry.target.classList.remove("opacity-100");
-    }
-  });
-}
+const formGreeting = document.querySelector(".form");
 
-const observer = new IntersectionObserver(handleIntersection, {
-  root: null, // Use the viewport as the root
-  rootMargin: "0px", // No margin
-  threshold: 0.5, // Trigger when 50% of the element is visible
+formGreeting.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const nameInput = document.getElementById("username");
+  const greetingInput = document.getElementById("ucapan");
+
+  const name = nameInput.value;
+  const greeting = greetingInput.value;
+
+  // Construct URL with form data
+  const apiUrl = `https://jalanjalin-api.onrender.com/add_greeting?name=${encodeURIComponent(
+    name
+  )}&greeting=${encodeURIComponent(greeting)}`;
+
+  // Make GET request to API
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+
+      fetchChatData();
+      nameInput.value = "";
+      greetingInput.value = "";
+    })
+    .catch((error) => {
+      // Handle errors
+      console.error("Error:", error);
+      // Display error message or handle error gracefully
+    });
 });
 
-// const greetingElement = document.getElementById("greeting");
-// observer.observe(greetingElement);
-
-// FETCH UCAPAN
-fetch("https://jalanjalin-api.onrender.com/all_greetings")
-  .then((response) => response.json())
-  .then((res) => {
-    const datas = res.data;
-    let chats = "";
-    datas.forEach((chat) => {
-      const dateString = chat.datetime;
-      const date = moment(dateString);
-      const formattedDate = date.format("DD MMM YYYY HH:mm");
-      chats += `<div class="font-poppins item-chat flex justify-start gap-2.5 mb-5">
+function fetchChatData() {
+  // Fetch chat data
+  fetch("https://jalanjalin-api.onrender.com/all_greetings")
+    .then((response) => response.json())
+    .then((res) => {
+      const datas = res.data;
+      let chats = "";
+      datas.forEach((chat) => {
+        const dateString = chat.datetime;
+        const date = moment(dateString);
+        const formattedDate = date.format("DD MMM YYYY HH:mm");
+        chats += `<div class="font-poppins item-chat flex justify-start gap-2.5 mb-5">
           <div
             class="flex flex-col w-full max-w-[320px] leading-1.5 p-4 bg-whiten shadow rounded-e-xl rounded-es-xl"
           >
@@ -68,37 +80,15 @@ fetch("https://jalanjalin-api.onrender.com/all_greetings")
             </p>
           </div>
         </div>`;
-    });
-    console.log(chats);
-    document.getElementById("chats").innerHTML = chats;
-  })
-  .catch((error) => console.log(error));
-
-// ADD UCAPAN
-const formGreeting = document.querySelector(".form");
-formGreeting.addEventListener("submit", (event) => {
-  const name = document.getElementById("username").value;
-  const greeting = document.getElementById("ucapan").value;
-
-  // Construct URL with form data
-  const apiUrl = `https://jalanjalin-api.onrender.com/add_greeting?name=${encodeURIComponent(
-    name
-  )}&greeting=${encodeURIComponent(greeting)}`;
-
-  // Make GET request to API
-  fetch(apiUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      // Handle response data if needed
-      console.log(data);
-      // For example, you can display a success message or update UI
+      });
+      console.log(chats);
+      document.getElementById("chats").innerHTML = chats;
     })
-    .catch((error) => {
-      // Handle errors
-      console.error("Error:", error);
-      // Display error message or handle error gracefully
-    });
-});
+    .catch((error) => console.log(error));
+}
+
+// Initial fetch of chat data
+fetchChatData();
 
 document.addEventListener("DOMContentLoaded", function () {
   const backgroundMusic = document.getElementById("backgroundMusic");
