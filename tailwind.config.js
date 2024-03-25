6aff982211922f015e3e2ec08bbf1c89ceff63f4
ouraan <rizhaalfianita1412@gmail.com>
2024-03-25 10:47:29 +0700

    add animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -72,7 +72,7 @@ module.exports = {
         slideInFromTop: {
           "0%": {
             opacity: "0",
-            transform: "translateY(-100%)",
+            transform: "translateY(-10%)",
           },
           "100%": {
             opacity: "1",
@@ -82,7 +82,7 @@ module.exports = {
         slideInFromBottom: {
           "0%": {
             opacity: "0",
-            transform: "translateY(100%)",
+            transform: "translateY(10%)",
           },
           "100%": {
             opacity: "1",
@@ -137,6 +137,7 @@ module.exports = {
     addDynamicIconSelectors(),
     require("@tailwindcss/aspect-ratio"),
     require("flowbite/plugin"),
+    require("tailwindcss-intersect"),
     // require("tailwindcss-animate"),
   ],
 };
